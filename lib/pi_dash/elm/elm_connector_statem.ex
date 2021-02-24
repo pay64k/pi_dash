dc697c8c861051885aeb2a8cd966f693b8d73086
pay64k <pay64k@gmail.com>
2021-02-24 15:47:41 +0100

    handle supported pids

--- lib/pi_dash/elm/elm_connector_statem.ex
+++ lib/pi_dash/elm/elm_connector_statem.ex
@@ -2,9 +2,11 @@ defmodule Elm.Data do
   defstruct [
     :port,
     :uart_port_pid,
-    :elm_opts,
+    :elm_queue,
     :elm_version,
-    :last_sent_at_command
+    :last_sent_command,
+    :protocol_number,
+    supported_pids: []
   ]
 end
 
@@ -29,12 +31,17 @@ defmodule Elm.ConnectorStatem do
     # no whitespaces
     "S0",
     # Describe the Protocol by Number
-    "DPN",
-    # PIDs supported [01 - 20]
-    "0100"
+    "DPN"
   ]
 
   @elm_device_name "Prolific"
+
+  @obd_pids_supported [
+    # PIDs supported [01 - 20] for 'Show current data' mode
+    "0100",
+    # PIDs supported [01 - 20] 'Request vehicle information' mode
+    "0900"
+  ]
 
   def write_at_command(msg) do
     GenStateMachine.cast(__MODULE__, {:write, "AT " <> msg})
@@ -51,7 +58,7 @@ defmodule Elm.ConnectorStatem do
   def init(_) do
     {:ok, uart_port_pid} = Circuits.UART.start_link()
     GenStateMachine.cast(__MODULE__, :open_connection)
-    {:ok, :connect, %Data{elm_opts: @elm_opts, uart_port_pid: uart_port_pid}}
+    {:ok, :connect, %Data{elm_queue: @elm_opts, uart_port_pid: uart_port_pid}}
   end
 
   def handle_event(
@@ -62,7 +69,7 @@ defmodule Elm.ConnectorStatem do
       ) do
     Logger.error("ELM disconnected on #{port} in state: #{state}")
     GenStateMachine.cast(__MODULE__, :open_connection)
-    {:next_state, :connect, %Data{data | elm_opts: @elm_opts}}
+    {:next_state, :connect, %Data{data | elm_queue: @elm_opts}}
   end
 
   def handle_event(:info, {:circuits_uart, port, ""}, _state, _data = %Data{port: port}) do
@@ -87,7 +94,7 @@ defmodule Elm.ConnectorStatem do
     case open_serial(data.uart_port_pid) do
       {:ok, port} ->
         write_at_command("Z")
-        {:next_state, :configuring, %{data | port: port, last_sent_at_command: "Z"}}
+        {:next_state, :configuring, %{data | port: port, last_sent_command: "Z"}}
 
       :error ->
         Process.sleep(5000)
@@ -98,27 +105,55 @@ defmodule Elm.ConnectorStatem do
 
   defp handle_msg(msg, :configuring, data) do
     cond do
-      String.contains?(msg, "Z") and data.last_sent_at_command == "Z" ->
+      String.contains?(msg, "Z") and data.last_sent_command == "Z" ->
         :keep_state_and_data
 
-      String.contains?(msg, "ELM") and data.last_sent_at_command == "Z" ->
+      String.contains?(msg, "ELM") and data.last_sent_command == "Z" ->
         Logger.info("Connected ELM version: #{msg}")
-        {to_send, rest} = List.pop_at(data.elm_opts, 0)
+        {to_send, rest} = List.pop_at(data.elm_queue, 0)
         write_at_command(to_send)
 
         {:next_state, :configuring,
-         %Data{data | elm_version: msg, last_sent_at_command: to_send, elm_opts: rest}}
+         %Data{data | elm_version: msg, last_sent_command: to_send, elm_queue: rest}}
 
       String.contains?(msg, "OK") ->
-        {to_send, rest} = List.pop_at(data.elm_opts, 0)
+        {to_send, rest} = List.pop_at(data.elm_queue, 0)
         write_at_command(to_send)
-        {:next_state, :configuring, %Data{data | last_sent_at_command: to_send, elm_opts: rest}}
+        {:next_state, :configuring, %Data{data | last_sent_command: to_send, elm_queue: rest}}
 
-      # TODO: handle going to connected_configured (get available pids, then start PidSup etc)
+      String.contains?(msg, "A") and data.last_sent_command == "DPN" ->
+        {to_send, rest} = List.pop_at(@obd_pids_supported, 0)
+        write_command(to_send)
+        {:next_state, :get_supported_pids, %Data{data | protocol_number: msg, last_sent_command: to_send, elm_queue: rest}}
 
       true ->
         :keep_state_and_data
     end
+  end
+
+  defp handle_msg(msg, :get_supported_pids, data) do
+    cond do
+      String.contains?(msg, "SEARCHING") ->
+        :keep_state_and_data
+
+      String.contains?(msg, "UNABLE TO CONNECT") ->
+        Logger.error("Unable to connect to vehicle ECU! Restarting...")
+        GenStateMachine.cast(__MODULE__, :open_connection)
+        {:next_state, :connect, %Data{data | elm_queue: @elm_opts}}
+
+      data.last_sent_command == "0100" ->
+        Logger.info("Supported PIDs for mode 01 (show current data): #{inspect msg}")
+        {to_send, rest} = List.pop_at(@obd_pids_supported, 0)
+        write_command(to_send)
+        {:next_state, :get_supported_pids, %Data{data | supported_pids: data.supported_pids ++ [msg], elm_queue: rest}}
+
+      data.last_sent_command == "0900" ->
+        Logger.info("Supported PIDs for mode 09 (show current data): #{inspect msg}")
+        # TODO: handle going to connected_configured (get available pids, then start PidSup etc)
+
+        {:next_state, :connected_configured, %Data{data | supported_pids: data.supported_pids ++ [msg]}}
+    end
+
   end
 
   defp prepare_received(msg) do
