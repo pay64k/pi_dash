0bd38e36e4ef47321a161f7839dcf566f933c0a6
pay64k <pay64k@gmail.com>
2021-02-28 20:06:46 +0100

    add clock

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,7 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import "../css/app.scss"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
 import socket from "./socket"
-//
+
 import "phoenix_html"
 
 var loadingioBar = require("loadingio-bar")
@@ -19,6 +9,9 @@ var loadingioBar = require("loadingio-bar")
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const ReactOnPhoenix = () => <div>This is a React Component!!!</div>
+import Clock from './components/clock'
 
-ReactDOM.render(<ReactOnPhoenix />, document.getElementById('mountPoint'))
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
---
+++ assets/js/components/clock.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h2>{this.state.date.toLocaleTimeString()}</h2>
+        </div>
+      );
+    }
+   fuck() {
+      console.log("fcuk")
+    }
+  }
+
+  export default Clock
