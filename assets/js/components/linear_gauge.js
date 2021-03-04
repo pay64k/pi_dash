0ffb4fa937e465485a27b6a0149f772857834b87
pay64k <pay64k@gmail.com>
2021-03-04 22:29:06 +0100

    round down calculations

--- assets/js/components/linear_gauge.js
+++ assets/js/components/linear_gauge.js
@@ -22,7 +22,7 @@ class LinearGauge extends React.Component {
     }
 
     calculateWidth(value, canvas) {
-        return value * canvas.width / this.props.max_value;
+        return Math.floor(value * canvas.width / this.props.max_value);
         // (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
