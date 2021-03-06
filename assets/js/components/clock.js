66b417cb6d801f96d76b2cc84ba387ef65ec0399
pay64k <pay64k@gmail.com>
2021-03-06 12:59:00 +0100

    smaller font

--- assets/js/components/clock.js
+++ assets/js/components/clock.js
@@ -26,7 +26,7 @@ class Clock extends React.Component {
     render() {
       return (
         <div>
-          <h2>{this.state.date.toLocaleTimeString()}</h2>
+          <h4>{this.state.date.toLocaleTimeString()}</h4>
         </div>
       );
     }
