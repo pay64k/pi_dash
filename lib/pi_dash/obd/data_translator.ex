f548bd680c16ff01c477017ffeb231486a375be5
pay64k <pay64k@gmail.com>
2021-03-09 20:57:49 +0100

    handle empty engine load msg

--- lib/pi_dash/obd/data_translator.ex
+++ lib/pi_dash/obd/data_translator.ex
@@ -23,9 +23,10 @@ defmodule Obd.DataTranslator do
   def handle_data(:speed, <<a, _crc>>), do: a
 
   def handle_data(:engine_load, <<a, _crc>>), do: 100 / 255 * a
+  def handle_data(:engine_load, <<>>), do: 0
 
   def handle_data(:coolant_temp, <<a, _crc>>), do: a - 40
-  
+
   def handle_data(:intake_temp, <<a, _crc>>), do: a - 40
 
   def handle_data(_obd_pid, _data), do: {:error, :unhandled}
